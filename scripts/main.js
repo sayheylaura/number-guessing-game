25e4d6a7f3c43c73932b5edd78e707a2f2904071
lauraferrandof <lauraferrandof@gmail.com>
2018-12-03 18:03:27 +0100

    Store random number result in a constant

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,7 @@ const tipBoxElement = document.querySelector('.tip-box');
 
 const tryMeterElement = document.querySelector('.try-meter');
 
-function handleSubmitBtn(event) {
-    event.preventDefault();
-    console.log(userNumberElement.value);
-}
-
-submitBtnElement.addEventListener('click', handleSubmitBtn);
-
+const generatedRandomNumber = getRandomNumber(100);
 
 // Función que genera un número aleatorio hasta un máximo dado
 function getRandomNumber(max) {
@@ -24,3 +18,16 @@ function getRandomNumber(max) {
 // Se pinta en la consola un número random hasta 100
 console.log('> ' + getRandomNumber(100));
 
+function handleSubmitBtn(event) {
+    event.preventDefault();
+
+    if (parseInt(userNumberElement.value) === generatedRandomNumber) {
+        tipBoxElement.innerHTML = '¡HAS GANADO, CAMPEONA!';
+    } else if (parseInt(userNumberElement.value) < generatedRandomNumber) {
+        tipBoxElement.innerHTML = 'Demasiado bajo';
+    } else if (parseInt(userNumberElement.value) > generatedRandomNumber) {
+        tipBoxElement.innerHTML = 'Demasiado alto';
+    }
+}
+
+submitBtnElement.addEventListener('click', handleSubmitBtn);
