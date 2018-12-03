b6a8c3b16402e6365f73cae4a3da6a604c49b0b0
lauraferrandof <lauraferrandof@gmail.com>
2018-12-03 17:22:51 +0100

    Start js

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,21 @@
 'use strict';
 
+const userNumberElement = document.querySelector('.user-number');
+
+const submitBtnElement = document.querySelector('.submit-btn');
+
+function handleSubmitBtn(event) {
+    
+}
+
+submitBtnElement.addEventListener('click', handleSubmitBtn);
+
+
+// Función que genera un número aleatorio hasta un máximo dado
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+// Se pinta en la consola un número random hasta 100
+console.log('> ' + getRandomNumber(100));
+
