c46a01f64ea04997fe14dc9cd155410b993c4e0c
lauraferrandof <lauraferrandof@gmail.com>
2018-12-03 17:39:14 +0100

    Write basic function

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,13 @@ const userNumberElement = document.querySelector('.user-number');
 
 const submitBtnElement = document.querySelector('.submit-btn');
 
+const tipBoxElement = document.querySelector('.tip-box');
+
+const tryMeterElement = document.querySelector('.try-meter');
+
 function handleSubmitBtn(event) {
-    
+    event.preventDefault();
+    console.log(userNumberElement.value);
 }
 
 submitBtnElement.addEventListener('click', handleSubmitBtn);
