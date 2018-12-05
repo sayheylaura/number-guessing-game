cba019f49b39c8adb571a53f7f0b78a0e22069a7
lauraferrandof <lauraferrandof@gmail.com>
2018-12-05 13:33:41 +0100

    Define the counter logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,18 @@
 'use strict';
 
 // Recojo los elementos HTML que necesito
-const userNumberElement = document.querySelector('.user-number');
+const userNumberEl = document.querySelector('.user-number');
 
-const submitBtnElement = document.querySelector('.submit-btn');
+const submitBtnEl = document.querySelector('.submit-btn');
 
-const tipBoxElement = document.querySelector('.tip-box');
+const tipBoxEl = document.querySelector('.tip-box');
 
-const attemptsMeterElement = document.querySelector('.attempts-meter');
+const attemptsMeterEl = document.querySelector('.attempts-meter');
 
 // Guardo el número aleatorio generado en una constante
 const generatedRandomNumber = getRandomNumber(100);
 
-// Declaro una variable que necesito para contar cuántas veces se pulsa el botón submit
+// Acumulador: declaro una variable que necesito para contar cuántas veces se pulsa el botón submit
 let acc = 0;
 
 // Muestro en la consola el número aleatorio generado al cargarse la página. Como logueo la constante en la que lo he guardado, solo se genera un número cada vez
@@ -25,27 +25,29 @@ function getRandomNumber(max) {
 
 // Función que compara el número que introduce la usuaria con el número aleatorio generado. En función de la respuesta, se pinta un feedback diferente en la caja de pistas
 function compareNumbers() {
-    const userNumberValue = parseInt(userNumberElement.value);
+    const userNumberValue = parseInt(userNumberEl.value);
     console.log(userNumberValue);
 
     if (userNumberValue === generatedRandomNumber) {
-        tipBoxElement.innerHTML = '¡HAS GANADO, CAMPEONA!';
+        tipBoxEl.innerHTML = '¡HAS GANADO, CAMPEONA!';
     } else if (userNumberValue < generatedRandomNumber) {
-        tipBoxElement.innerHTML = 'Demasiado bajo';
+        tipBoxEl.innerHTML = 'Demasiado bajo';
     } else {
-        tipBoxElement.innerHTML = 'Demasiado alto';
+        tipBoxEl.innerHTML = 'Demasiado alto';
     }
 }
 
-// Función que compara el número que introduce la usuaria con el número aleatorio generado. Es un callback que se ejecuta cuando la usuaria pulsa el botón "Prueba"
+// Función callback que se ejecuta cuando la usuaria pulsa el botón "Prueba"
 function handleSubmitBtn(event) {
+    // Evito que al pulsar el botón "Prueba" se recargue la página (funcionalidad por defecto del type "submit")
     event.preventDefault();
 
+    // Ejecuto la función que compara el número introducido por la usuaria y el número aleatorio, y que pinta un feedback distinto dependiendo del caso
     compareNumbers();
 
-    //Necesito almacenar en acc el número de veces que se pulsa el botón de submit, y luego recoger eso en el attemptsMeterElement
-
-    //attemptsMeterElement.innerHTML = numberOfAttempts;
+    //Contador: cada vez que se pulsa el botón "Prueba", el acumulador suma 1 y su valor se pinta en la caja HTML del contador
+    acc += 1;
+    attemptsMeterEl.innerHTML = acc;
 }
 
-submitBtnElement.addEventListener('click', handleSubmitBtn);
+submitBtnEl.addEventListener('click', handleSubmitBtn);
